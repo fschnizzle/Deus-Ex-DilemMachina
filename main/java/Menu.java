c0296af673ee55a98ae70890754acaeea19fbb78
fschnizzle <fschneider@student.unimelb.edu.au>
2023-06-10 17:33:42 +1000

    Initial Commit
    RescueBot:
    - printMessage() helper function reads from text files (.txt, .ascii, etc) and prints contents to standard output
    - calls Menu class
    
    Menu:
    - runMenuLoop() is the main for the menu, loops until 'q' or 'quit' given
    - displayMenu() prints the options available for user selections
    - handleUserInput() will handle the different options selected by the user ['q','j','a','r']
    
    Outliner details a rough draft of the classes / directories being implemented (in PlantUML format) and as a repository skeleton

---
+++ main/java/Menu.java
@@ -0,0 +1,36 @@
+package main.java;
+
+import java.util.Scanner;
+
+public class Menu {
+    private Scanner keyboard;
+
+    // Constructor
+    public Menu() {
+        this.keyboard = new Scanner(System.in);
+    }
+
+    public void displayMenu() {
+        // Print out the menu options
+        System.out.println("Please enter one of the following commands to continue:");
+        System.out.println("- judge scenarios: [judge] or [j]");
+        System.out.println("- run simulations with the in-built decision algorithm: [run] or [r]");
+        System.out.println("- show audit from history: [audit] or [a]");
+        System.out.print("- quit the program: [quit] or [q]\n> ");
+    }
+
+    public void runMenuLoop() {
+        String input;
+        do {
+            displayMenu();
+            input = keyboard.nextLine().trim();
+            handleUserInput(input);
+        } while (!input.equals("q") && !input.equals("quit"));
+    }
+
+    public void handleUserInput(String input) {
+        // Switch statement to handle different user input cases
+        System.out.print("You entered: " + input + "\n");
+
+    }
+}
---
+++ main/java/RescueBot.java
@@ -0,0 +1,71 @@
+package main.java;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.Math;
+
+/**
+ * COMP90041, Sem1, 2023: Final Project
+ * 
+ * @author: Flynn Schneider
+ *          student id: 982143
+ *          student email: fschneider@student.unimelb.edu.au
+ */
+public class RescueBot {
+
+    private int scenariosImportedCount;
+
+    /**
+     * Decides whether to save the passengers or the pedestrians
+     * 
+     * @param Scenario scenario: the ethical dilemma
+     * @return Decision: which group to save
+     */
+
+    // Decision Static Method
+
+    // public static Location decide(Scenario scenario) {
+    // // a very simple decision engine
+    // // TODO: take into account at least 5 characteristics
+
+    // // 50/50
+    // if (Math.random() > 0.5) {
+    // return scenario.getLocation(1);
+    // } else {
+    // return scenario.getLocation(2);
+    // }
+    // }
+
+    public static void printMessage(String filePath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            System.out.println("");
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file: " + e.getMessage());
+        }
+    }
+
+    /*
+     * Program entry
+     */
+    public static void main(String[] args) {
+        // Print the welcome message
+        String welcomeFileName = "main/java/etc/welcome.ascii";
+        printMessage(welcomeFileName);
+
+        // Load the scenarios
+        // Print out the number of loaded scenarios
+        System.out.println("{N} scenarios imported.");
+
+        // Initiate Menu Loop
+        Menu menu = new Menu();
+        menu.runMenuLoop();
+
+        // Exit the program
+        System.exit(0);
+    }
+}
